00a4cfd4f77005a027a56da3ccc8f0330a508c5c
Ashmit1014 <2023kucp1014@iiitkota.ac.in>
2023-12-30 22:16:32 +0530

    Lab Programs

---
+++ Program_3/Program3.c
@@ -0,0 +1,12 @@
+/*3. Ramesh’s basic salary is input through the keyboard. His dearness allowance is 40% of basic salary, and house rent allowance is 20% of basic salary. Write a program to calculate his gross salary.*/
+#include<stdio.h>
+int main()
+{
+    float b_salary,da,hra,g_salary;
+    printf("Enter the basic salary= ");
+    scanf("%f",&b_salary);
+    da=(40*b_salary)/100;
+    hra=(20*b_salary)/100;
+    g_salary=b_salary+da+hra;
+    printf("The gross salary= %f",g_salary);
+}
